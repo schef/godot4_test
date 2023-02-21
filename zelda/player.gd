adfcd18bcbcffd24e620ca6b5d29b6e4d02c15ba
schef <zvanstefan@gmail.com>
2023-02-21 13:44:58 +0100

    zezancija

--- zelda/player.gd
+++ zelda/player.gd
@@ -1,9 +1,20 @@
 extends CharacterBody2D
 
-
 const move_speed: int = 100
 
 @onready var animation_tree: AnimationTree = %AnimationTree
+@onready var area: Area2D = $Area2D
+
+@export var pada: bool = false
+
+func on_drop(area: Area2D):
+	print(area.name)
+	if area.name == "drop":
+		pada = true
+
+func _ready():
+	area.connect("area_entered", on_drop)
+	
 
 func _physics_process(delta):
 	var input_direction = Vector2(
@@ -14,11 +25,20 @@ func _physics_process(delta):
 	if input_direction != Vector2.ZERO:
 		animation_tree.set("parameters/idle/blend_position", input_direction)
 		animation_tree.set("parameters/walk/blend_position", input_direction)
-	velocity = input_direction * move_speed
+		
+		
+	if not pada:
+		velocity = input_direction * move_speed
+	else:
+		velocity.y = 200
+		velocity.x = 0
+		animation_tree.set("parameters/idle_or_walk/blend_amount", 0)
 	
 	if velocity != Vector2.ZERO:
 		animation_tree.set("parameters/idle_or_walk/blend_amount", 1)
 	else:
 		animation_tree.set("parameters/idle_or_walk/blend_amount", 0)
+	
+	
 	
 	move_and_slide()
